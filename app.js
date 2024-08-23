f58c912463926dec53a8040b0b16881244b5acdf
Atindra Kumar Singh <atindra.singh86@gmail.com>
2024-08-23 18:31:22 -0400

    2nd commit

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-// app.js
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const port = process.env.PORT || 3000,
+    http = require('http'),
+    fs = require('fs'),
+    html = fs.readFileSync('index.html');
 
-const app = express();
-const port = 3000;
+const log = function(entry) {
+    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+};
 
-// Route for root path
-app.get("/", (req, res) => {
-  const indexPath = path.join(__dirname, "index.html");
-  fs.readFile(indexPath, "utf8", (err, data) => {
-    if (err) {
-      res.status(500).send("Sorry, something went wrong.");
-      return;
+const server = http.createServer(function (req, res) {
+    if (req.method === 'POST') {
+        let body = '';
+
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            if (req.url === '/') {
+                log('Received a message.');
+            } else if (req.url = '/scheduled') {
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            }
+
+            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+            res.end();
+        });
+    } else {
+        res.writeHead(200);
+        res.write(html);
+        res.end();
     }
-    res.send(data);
-  });
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log('Server running at http://127.0.0.1:' + port + '/');
